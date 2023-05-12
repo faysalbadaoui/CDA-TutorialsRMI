55c7c589aaf50247928164d717580a3745c45d16
Faysal Badaoui <faysalbadaoui02@gmail.com>
2023-05-12 15:49:38 +0200

    implemented new client

--- Tutorial5.2/SumatorioPrimos/Client.java
+++ Tutorial5.2/SumatorioPrimos/Client.java
@@ -1,20 +1,61 @@
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
-public class Client {
+public class Client extends UnicastRemoteObject implements Worker {
+    private PrimeSumService service;
+
+    public Client(PrimeSumService service) throws RemoteException {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public int processSubtask(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+
+    @Override
+    public void notifyCompletion() {
+        System.out.println("Calculation completed");
+    }
+
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             PrimeSumService service = (PrimeSumService) registry.lookup("PrimeSumService");
-            // Asynchronous call
-            service.asyncPrimeSum(Integer.parseInt(args[0]));
+
+            Client client = new Client(service);
+            service.registerWorker(client);
+
+            service.asyncPrimeSum(10);
             while(!service.isCompleted()) {
                 System.out.println("Waiting for calculation to complete...");
                 Thread.sleep(1000);
             }
-            int asyncSum = service.getLatestSum();
-            System.out.println("Asynchronous prime sum: " + asyncSum);
+            int sum = service.getLatestSum();
+            System.out.println("Asynchronous distributed prime sum: " + sum);
 
+            service.unregisterWorker(client);
         } catch (Exception e) {
             e.printStackTrace();
         }
